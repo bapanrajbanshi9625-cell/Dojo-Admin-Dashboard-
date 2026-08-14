de7e1f5245b6f16231e76498dcb8d7940e94cca6
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-14 21:30:02 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,9 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    throw UnsupportedError(
-      'Firebase configuration has not been generated yet. '
-      'Run FlutterFire CLI to generate firebase_options.dart.',
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyBKGx8iof7xv0h666Tdm7I0sddJIZ3ti44',
+      authDomain: 'dojo-platform-a5dc8.firebaseapp.com',
+      databaseURL:
+          'https://dojo-platform-a5dc8-default-rtdb.asia-southeast1.firebasedatabase.app',
+      projectId: 'dojo-platform-a5dc8',
+      storageBucket: 'dojo-platform-a5dc8.firebasestorage.app',
+      messagingSenderId: '719463503810',
+      appId: '1:719463503810:web:bd2c9f06df631e8200a85b',
+      measurementId: 'G-56Z1BRCL0Q',
     );
   }
 }
