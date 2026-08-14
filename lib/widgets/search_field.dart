d738ea9a8156b177a0f4f219adb57728896ba33b
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-14 19:51:38 +0530

    Update search_field.dart

--- lib/widgets/search_field.dart
+++ lib/widgets/search_field.dart
@@ -1,1 +1,82 @@
+import 'package:flutter/material.dart';
 
+const Color dojoOrange = Color(0xFFD35435);
+const Color dojoBlack = Color(0xFF263238);
+const Color dojoGrey = Color(0xFF6B7280);
+const Color dojoBorder = Color(0xFFE7E9ED);
+
+class DojoSearchField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController? controller;
+  final ValueChanged<String>? onChanged;
+  final VoidCallback? onClear;
+
+  const DojoSearchField({
+    super.key,
+    this.hintText = 'Search...',
+    this.controller,
+    this.onChanged,
+    this.onClear,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: dojoBorder),
+      ),
+      child: TextField(
+        controller: controller,
+        onChanged: onChanged,
+        style: const TextStyle(
+          fontSize: 12,
+          color: dojoBlack,
+          fontWeight: FontWeight.w600,
+        ),
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            color: dojoGrey,
+            fontSize: 12,
+          ),
+          prefixIcon: const Icon(
+            Icons.search,
+            size: 20,
+            color: dojoGrey,
+          ),
+          suffixIcon: controller != null
+              ? ValueListenableBuilder<TextEditingValue>(
+                  valueListenable: controller!,
+                  builder: (context, value, child) {
+                    if (value.text.isEmpty) {
+                      return const SizedBox.shrink();
+                    }
+
+                    return IconButton(
+                      onPressed: onClear ??
+                          () {
+                            controller!.clear();
+                            onChanged?.call('');
+                          },
+                      icon: const Icon(
+                        Icons.close,
+                        size: 18,
+                      ),
+                      color: dojoGrey,
+                    );
+                  },
+                )
+              : null,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 13,
+          ),
+        ),
+      ),
+    );
+  }
+}
