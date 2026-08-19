2d4141142f24335d073fcf940d36ebd148fef2b9
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-20 02:45:45 +0530

    Update import from walkers_details_helpers to walkers_helpers

--- lib/features/walkers/widgets/walkers_details_section.dart
+++ lib/features/walkers/widgets/walkers_details_section.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'walkers_details_helpers.dart';
+import 'walkers_helpers.dart';
 
 class WalkerDetailsSection extends StatelessWidget {
   final String title;
