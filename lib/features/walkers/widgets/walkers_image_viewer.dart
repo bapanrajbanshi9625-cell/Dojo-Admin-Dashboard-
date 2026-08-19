6278b67ab7e7c34b6b0a5030ad31acc90b99b648
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-20 02:48:09 +0530

    Update import from walkers_details_helpers to walkers_helpers

--- lib/features/walkers/widgets/walkers_image_viewer.dart
+++ lib/features/walkers/widgets/walkers_image_viewer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'walkers_details_helpers.dart';
+import 'walkers_helpers.dart';
 
 /// ============================================================
 /// WALKER IMAGE VIEWER
